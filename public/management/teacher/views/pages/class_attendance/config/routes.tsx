ae874168b7815095d909bc81bded899d51040b4a
Yamin Hossain <streatdot616@gmail.com>
2024-06-24 18:57:28 +0600

    last updated

---
+++ public/management/teacher/views/pages/class_attendance/config/routes.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from './Layout';
+import Index from '../Index';
+import Result from '../Result';
+import TakeAttendace from '../pages/TakeAttendance';
+
+// export { default as DashboardCounterAll} from "./All.jsx";
+
+export default {
+    path: 'class-attendance',
+    element: <Layout />,
+    children: [
+        {
+            path: '',
+            index: true,
+            element: <Index />,
+        },
+        {
+            path: 'details',
+            element: <Result />,
+        },
+        {
+            path: 'take-attendance',
+            element: <TakeAttendace />,
+        },
+    ],
+};
---
+++ public/management/teacher/views/pages/class_attendance/pages/TakeAttendance.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export interface Props {}
+
+const TakeAttendance: React.FC<Props> = (props: Props) => {
+    interface data {
+        [key: string]: any;
+    }
+    const datas: data[] = [
+        {
+            id: 1,
+            name: 'Shahin',
+            grade: 'A',
+        },
+        {
+            id: 2,
+            name: 'Tamim',
+            grade: 'A+',
+        },
+        {
+            id: 3,
+            name: 'Ramim',
+            grade: 'A+',
+        },
+    ];
+
+    return (
+        <div className="admin_dashboard">
+            <div className="result_details">
+                <h3 className="table_heading mt-4">First Semester</h3>
+                <form action="" onSubmit={(e) => e.preventDefault()}>
+                    <div className="teacher_result">
+                        <div>
+                            <div>Class</div>
+                            <div>
+                                <select name="class" id="">
+                                    <option value="six">Six</option>
+                                    <option value="seven">Seven</option>
+                                    <option value="eight">Eight</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                            <div>Date</div>
+                            <div>
+                                <input type="date" name="" id="" />
+                            </div>
+                        </div>
+                        <div>
+                            <div>Roll</div>
+                            <div>
+                                <input type="number" name="roll" id="" />
+                            </div>
+                        </div>
+                        <button
+                            className="btn btn-sm btn-outline-info "
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className="content_body">
+                <div className="data_list">
+                    <div className="table_responsive custom_scroll">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Serial</th>
+                                    <th>Name</th>
+                                    <th>Check box</th>
+                                </tr>
+                            </thead>
+                            <tbody id="all_list">
+                                {datas?.map((i: { [key: string]: any }) => {
+                                    return (
+                                        <tr>
+                                            <td></td>
+                                            <td>{i.id}</td>
+                                            <td>{i.name}</td>
+                                            <td>
+                                                <input
+                                                    type="checkbox"
+                                                    name=""
+                                                    id=""
+                                                />
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TakeAttendance;
