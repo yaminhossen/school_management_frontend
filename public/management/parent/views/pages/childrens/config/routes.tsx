c804a014341574057d7f7795ade0900fdf4551d6
Yamin Hossain <streatdot616@gmail.com>
2024-06-26 16:14:20 +0600

    parent children complete

--- public/management/parent/views/pages/childrens/config/routes.tsx
+++ public/management/parent/views/pages/childrens/config/routes.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import Layout from './Layout';
 import Index from '../Index';
 import SingleStudent from '../pages/SingleStudent';
+import AcademicInformation from '../pages/AcademicInformation';
+import BasicInformation from '../pages/BasicInformation';
+import Document from '../pages/Document';
+import Skills from '../pages/Skills';
+import Language from '../pages/Language';
+import ContactNumber from '../pages/ContactNumer';
+import EducationalBackground from '../pages/EducationalBackground';
+import Dues from '../pages/Dues';
+import Results from '../pages/Results';
+import Markshit from '../pages/Markshit';
+import Parents from '../pages/Parent';
+import Payments from '../pages/Payments';
 
 // export { default as DashboardCounterAll} from "./All.jsx";
 
@@ -17,6 +29,56 @@ export default {
         {
             path: 'details',
             element: <SingleStudent />,
+            children: [
+                {
+                    path: '',
+                    element: <BasicInformation />,
+                },
+                {
+                    path: 'academic-information',
+                    element: <AcademicInformation />,
+                },
+                {
+                    path: 'document',
+                    element: <Document />,
+                },
+                {
+                    path: 'skill',
+                    element: <Skills />,
+                },
+                {
+                    path: 'parent',
+                    element: <Parents />,
+                },
+                {
+                    path: 'language',
+                    element: <Language />,
+                },
+                {
+                    path: 'contact-number',
+                    element: <ContactNumber />,
+                },
+                {
+                    path: 'educational-background',
+                    element: <EducationalBackground />,
+                },
+                {
+                    path: 'dues',
+                    element: <Dues />,
+                },
+                {
+                    path: 'payments',
+                    element: <Payments />,
+                },
+                {
+                    path: 'result-part',
+                    element: <Results />,
+                },
+                {
+                    path: 'mark-sheet',
+                    element: <Markshit />,
+                },
+            ],
         },
     ],
 };
--- public/management/parent/views/pages/childrens/pages/SingleStudent.tsx
+++ public/management/parent/views/pages/childrens/pages/SingleStudent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 export interface Props {}
 
 const SingleStudent: React.FC<Props> = (props: Props) => {
@@ -20,53 +20,105 @@ const SingleStudent: React.FC<Props> = (props: Props) => {
 
     return (
         <div className="admin_dashboard">
-            <div className="content_body">
-                <div className="data_list">
-                    <div className="table_responsive custom_scroll">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Photo</th>
-                                    <th>Name</th>
-                                    <th>Roll</th>
-                                    <th>Class</th>
-                                    <th>CGPA</th>
-                                    <th>Present</th>
-                                    <th>Total Absance</th>
-                                </tr>
-                            </thead>
-                            <tbody id="all_list">
-                                {datas?.map((i: { [key: string]: any }) => {
-                                    return (
-                                        <tr>
-                                            <td></td>
-                                            <td>
-                                                <img
-                                                    className="children_img"
-                                                    src="/assets/dashboard/images/avatar.png"
-                                                    alt="teacher"
-                                                />
-                                            </td>
-                                            <td>{i.name}</td>
-                                            <td>{i.roll}</td>
-                                            <td>{i.class}</td>
-                                            <td>{i.cgpa}</td>
-                                            <td>{i.present}</td>
-                                            <td>{i.total_absance}</td>
-                                            {/* <td>
-                                                <Link
-                                                    className="btn btn-sm btn-outline-info mr-1"
-                                                    to="/student/single-student"
-                                                >
-                                                    Details
-                                                </Link>
-                                            </td> */}
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
+            <div className="admin_dashboard">
+                <div className="single-info-details">
+                    <div className="item-img">
+                        <img
+                            className="user_profile_img"
+                            src="/assets/dashboard/images/avatar.png"
+                            alt="teacher"
+                        />
+                    </div>
+                    <div className="item-content">
+                        <div className="header-inline item-header details_header">
+                            <h3 className="text-dark-medium profile_name font-medium">
+                                Shafiqur Rahman
+                            </h3>
+                            <div className="header-elements">
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <span className="material-symbols-outlined fill">
+                                                edit_square
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span className="material-symbols-outlined fill">
+                                                print
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span className="material-symbols-outlined fill">
+                                                system_update_alt
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <ul className="section_naviagation">
+                            <li>
+                                <Link to="/childrens/details">
+                                    Basic informations
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/academic-information">
+                                    Academic informations
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/document">
+                                    Documents
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/parent">
+                                    Parents
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/skill">
+                                    Skills
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/language">
+                                    Languages
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/contact-number">
+                                    Contact Numbers
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/educational-background">
+                                    Educational Backgrounds
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/payments">
+                                    Payments
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/dues">Dues</Link>
+                            </li>
+                            <li>
+                                <Link to="/childrens/details/result-part">
+                                    Result Part
+                                </Link>
+                            </li>
+                        </ul>
+                        <div></div>
+                        <div className="info-table table-responsive">
+                            <Outlet></Outlet>
+                        </div>
                     </div>
                 </div>
             </div>
