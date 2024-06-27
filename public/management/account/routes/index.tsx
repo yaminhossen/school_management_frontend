f88d3e13f9cd7060560beca8dfa074fed17b2235
Yamin Hossain <streatdot616@gmail.com>
2024-06-27 18:54:48 +0600

    accout

--- public/management/account/routes/index.tsx
+++ public/management/account/routes/index.tsx
@@ -6,6 +6,14 @@ import React from 'react';
 import profile_routes from '../views/pages/profile/config/routes';
 import income_entry_routes from '../views/pages/income_entry/config/routes';
 import expense_entry_routes from '../views/pages/expense_entry/config/routes';
+import accounts_routes from '../views/pages/accounts/config/routes';
+import account_category_routes from '../views/pages/account_category/config/routes';
+import credit_routes from '../views/pages/credit/config/routes';
+import debit_routes from '../views/pages/debit/config/routes';
+import income_statement_routes from '../views/pages/income_statement/config/routes';
+import journal_routes from '../views/pages/journal/config/routes';
+import ledger_routes from '../views/pages/ledger/config/routes';
+import due_list_routes from '../views/pages/due_list/config/routes';
 
 interface RouteTypes extends NonIndexRouteObject {}
 const router: RouteTypes[] = [
@@ -16,6 +24,14 @@ const router: RouteTypes[] = [
             profile_routes,
             income_entry_routes,
             expense_entry_routes,
+            accounts_routes,
+            account_category_routes,
+            credit_routes,
+            debit_routes,
+            income_statement_routes,
+            journal_routes,
+            ledger_routes,
+            due_list_routes,
             {
                 path: '',
                 element: <T1 />,
