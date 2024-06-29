4087b6e9f280b10ca697716f7ff7a1d36a904d64
Yamin Hossain <streatdot616@gmail.com>
2024-06-29 15:47:28 +0600

    credit and debit page create

--- public/management/account/views/pages/debit/config/routes.tsx
+++ public/management/account/views/pages/debit/config/routes.tsx
@@ -5,7 +5,7 @@ import Index from '../Index';
 // export { default as DashboardCounterAll} from "./All.jsx";
 
 export default {
-    path: 'due-list',
+    path: 'debit',
     element: <Layout />,
     children: [
         {
