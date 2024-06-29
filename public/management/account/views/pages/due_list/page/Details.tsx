8d1d00d4c45c3196f85515bede8840633e6cc0ab
Yamin Hossain <streatdot616@gmail.com>
2024-06-29 17:15:28 +0600

    create account categroy

--- public/management/account/views/pages/due_list/page/Details.tsx
+++ public/management/account/views/pages/due_list/page/Details.tsx
@@ -10,36 +10,26 @@ const Index: React.FC<Props> = (props: Props) => {
         {
             id: 1,
             last_date: '10 Feb, 2024',
-            income: '3000',
-            expense: '2000',
-            category: 'hostel bill',
+            amount: '3000',
+            purpose: 'Hostel bill',
         },
         {
             id: 2,
             last_date: '14 March, 2024',
-            income: '10000',
-            expense: '5000',
-            category: 'admission bill',
+            amount: '2000',
+            purpose: 'Transport bill',
         },
         {
             id: 3,
             last_date: '15 Feb, 2024',
-            income: '5000',
-            expense: '1000',
-            category: 'transport bill',
+            amount: '4000',
+            purpose: 'Couching bill',
         },
     ];
 
     return (
         <div className="admin_dashboard">
             <div className="content_body">
-                <Link
-                    to="/account-category/create"
-                    className="btn btn-sm btn-outline-info mb-2"
-                    type="submit"
-                >
-                    Create category
-                </Link>
                 <div className="data_list">
                     <div className="table_responsive custom_scroll">
                         <table>
@@ -47,9 +37,9 @@ const Index: React.FC<Props> = (props: Props) => {
                                 <tr>
                                     <th></th>
                                     <th>Serial</th>
-                                    <th>Category</th>
-                                    <th>Income</th>
-                                    <th>Expense</th>
+                                    <th>Perpouse</th>
+                                    <th>Last date</th>
+                                    <th>Amount</th>
                                 </tr>
                             </thead>
                             <tbody id="all_list">
@@ -58,20 +48,20 @@ const Index: React.FC<Props> = (props: Props) => {
                                         <tr>
                                             <td></td>
                                             <td>{i.id}</td>
-                                            <td>{i.category}</td>
-                                            <td>{i.income} tk</td>
-                                            <td>{i.expense} tk</td>
+                                            <td>{i.purpose}</td>
+                                            <td>{i.last_date}</td>
+                                            <td>{i.amount} tk</td>
                                         </tr>
                                     );
                                 })}
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Total:</td>
+                                    <td>9000 tk</td>
+                                </tr>
                             </tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td>Total:</td>
-                                <td>18000 tk</td>
-                                <td>8000 tk</td>
-                            </tr>
                         </table>
                     </div>
                 </div>
