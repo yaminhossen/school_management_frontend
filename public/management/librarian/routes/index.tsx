a1ed35943bf758cd7d9266631ffe5ccca1fc630c
Yamin Hossain <streatdot616@gmail.com>
2024-07-03 16:57:09 +0600

    librarian dashboard create

---
+++ public/management/librarian/routes/index.tsx
@@ -0,0 +1,24 @@
+import { NonIndexRouteObject } from 'react-router-dom';
+import DashboardLayout from '../views/layouts/DashboardLayout';
+import T1 from '../views/pages/T1';
+import user_branch_staff_routes from '../views/pages/users/config/routes';
+import React from 'react';
+import student_routes from '../views/pages/student/config/routes';
+
+interface RouteTypes extends NonIndexRouteObject {}
+const router: RouteTypes[] = [
+    {
+        path: '/',
+        element: <DashboardLayout />,
+        children: [
+            student_routes,
+            {
+                path: '',
+                element: <T1 />,
+            },
+            user_branch_staff_routes,
+        ],
+    },
+];
+
+export default router;
