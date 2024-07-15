83de5020b81b6d72854672165bb7edb8e7362868
Yamin Hossain <streatdot616@gmail.com>
2024-07-15 16:10:19 +0600

    employee salary and job position complete

--- src/modules/employee_salary_management/migrate.sh
+++ src/modules/employee_salary_management/migrate.sh
@@ -3,12 +3,28 @@
 # bash src/modules/employee_salary_management/migrate.sh
 
 
+# echo ""
+# echo "branch employee job pay grades seed start"
+# API_URL="http://127.0.0.1:5003/api/v1/branch-employee-job-pay-grades?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+# response=$(curl -s "$API_URL")
+# npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/employee_salary_management/branch_employee_job_pay_grades/models/seeders
+# echo "branch employee job pay grades seed end"
+# echo ""
+
+# echo ""
+# echo "branch employee job positions seed start"
+# API_URL="http://127.0.0.1:5003/api/v1/branch-employee-job-positions?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+# response=$(curl -s "$API_URL")
+# npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/employee_salary_management/branch_employee_job_positions/models/seeders
+# echo "branch employee job positions seed end"
+# echo ""
+
 echo ""
-echo "branch employee job pay grades seed start"
-API_URL="http://127.0.0.1:5003/api/v1/branch-employee-job-pay-grades?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+echo "branch employee salaries seed start"
+API_URL="http://127.0.0.1:5003/api/v1/branch-employee-salaries?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
 response=$(curl -s "$API_URL")
-npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/employee_salary_management/branch_employee_job_pay_grades/models/seeders
-echo "branch employee job pay grades seed end"
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/employee_salary_management/branch_employee_salaries/models/seeders
+echo "branch employee salaries seed end"
 echo ""
 
 
