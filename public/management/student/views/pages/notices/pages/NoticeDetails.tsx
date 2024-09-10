a0676681757d5b8a2a8fe24ddae3f9ddc309cd63
Yamin Hossain <streatdot616@gmail.com>
2024-09-10 19:05:18 +0600

    sdf

--- public/management/student/views/pages/notices/pages/NoticeDetails.tsx
+++ public/management/student/views/pages/notices/pages/NoticeDetails.tsx
@@ -1,43 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { app_config } from '../../../../../../../src/configs/app.config';
+import React, { useState, useEffect } from 'react';
+import { anyObject } from '../../../../common_types/object';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import moment from 'moment/moment';
 export interface Props {}
-
 const NoticeDetails: React.FC<Props> = (props: Props) => {
+    const [error, setError] = useState(null);
+    const [data, setData] = useState();
+    const { id } = useParams();
+
+    useEffect(() => {
+        // Function to fetch data
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`/api/v1/notices/${id}`);
+            setData(response.data.data);
+            // setData(response.data);
+        } catch (error) {
+            setError(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+    console.log(data);
     return (
         <div className="admin_dashboard">
             <div className="col-md-6">
-                <h3>Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nisi recusandae laboriosam assumenda perferendis mollitia!
-                    Reiciendis facere quo fuga nemo doloribus reprehenderit,
-                    minima consequatur mollitia veniam quis ipsa est modi
-                    repudiandae distinctio laborum iure illo in. Incidunt, quam
-                    corrupti tempore ipsam molestiae vitae asperiores quisquam
-                    eum eveniet sapiente? Rem debitis quam, dolorem perferendis
-                    voluptate quod inventore possimus sint veritatis facere ab
-                    architecto illum, eius fugiat soluta odio nesciunt alias,
-                    animi accusantium laborum dicta itaque? Ullam, accusantium
-                    incidunt ratione sint minima iusto explicabo ad labore
-                    excepturi! Consequuntur voluptatum reiciendis, quaerat
-                    repellat eos molestiae laudantium debitis illum autem qui
-                    dolores voluptas repudiandae id quo perferendis aliquam!
-                    Deleniti, possimus! Culpa dicta nisi eos ipsam totam
-                    repellendus quam aspernatur! Laboriosam id ab magnam
-                    doloremque deleniti et repellat perspiciatis? Modi facilis
-                    doloremque, hic numquam ipsum quod impedit distinctio
-                    molestias explicabo vitae excepturi eligendi enim velit.
-                    Commodi ullam incidunt similique recusandae iure repudiandae
-                    quibusdam laborum voluptate numquam officiis, dolor impedit
-                    accusamus eveniet adipisci distinctio? Doloribus, voluptates
-                    ipsum. Vel, maiores ipsam dolores, nulla repudiandae, est
-                    temporibus consequatur aliquam eius doloremque perspiciatis?
-                    Repudiandae labore sequi neque obcaecati blanditiis expedita
-                    vel! Ut dignissimos explicabo incidunt minus eligendi natus
-                    minima quisquam velit, saepe facilis soluta nesciunt, quidem
-                    ea quam sint ipsam.
-                </p>
+                <h3>{data?.title}</h3>
+                <p>{data?.description}</p>
             </div>
         </div>
     );
