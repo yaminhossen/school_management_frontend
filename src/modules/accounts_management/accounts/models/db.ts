e7e5eaf6662d29ff24d97a8a2274a1dd3feb39e0
Yamin Hossain <streatdot616@gmail.com>
2024-09-18 15:19:32 +0600

    klsdj

--- src/modules/accounts_management/accounts/models/db.ts
+++ src/modules/accounts_management/accounts/models/db.ts
@@ -3,6 +3,7 @@ import {
     Sequelize,
 } from 'sequelize';
 import * as accounts_model from './accounts_model';
+import * as account_logs_model from './account_logs_model';
 // import * as project_model from '../../user_admin copy/models/project_model';
 require('dotenv').config();
 
@@ -21,42 +22,26 @@ const sequelize = new Sequelize(
 
 interface models {
     AccontsModel: typeof accounts_model.DataModel;
+    AccountLogsModel: typeof account_logs_model.DataModel;
     // Project: typeof project_model.DataModel;
     sequelize: Sequelize;
 }
 const db = async function (): Promise<models> {
     const AccontsModel = accounts_model.init(sequelize);
+    const AccountLogsModel = account_logs_model.init(sequelize);
     // const Project = project_model.init(sequelize);
 
     await sequelize.sync();
 
-    // Project.hasOne(User, {
-    //     sourceKey: 'user_id',
-    //     foreignKey: 'id',
-    //     as: 'user',
-    // });
-
-    // User.hasMany(Project, {
-    //     sourceKey: 'id',
-    //     foreignKey: 'user_id',
-    //     as: 'projects',
-    // });
-
-    // User.hasOne(Project, {
-    //     sourceKey: 'id',
-    //     foreignKey: 'user_id',
-    //     as: 'project',
-    // });
-
-    // Project.belongsToMany(User, {
-    //     through: 'project_user',
-    // });
-    // User.belongsToMany(Project, {
-    //     through: 'project_user',
-    // });
+    AccontsModel.hasMany(AccountLogsModel, {
+        sourceKey: 'id',
+        foreignKey: 'account_id',
+        as: 'account_log',
+    });
 
     let models: models = {
         AccontsModel,
+        AccountLogsModel,
         // Project,
 
         sequelize,
--- src/modules/accounts_management/accounts/services/all_accounts.ts
+++ src/modules/accounts_management/accounts/services/all_accounts.ts
@@ -4,34 +4,63 @@ import { responseObject } from '../../../common_types/object';
 import response from '../helpers/response';
 import error_trace from '../helpers/error_trace';
 import custom_error from '../helpers/custom_error';
+import { Op, fn, col } from 'sequelize';
 
 async function details(
     fastify_instance: FastifyInstance,
     req: FastifyRequest,
 ): Promise<responseObject> {
-    let models = await db();
-    let params = req.params as any;
+    const models = await db();
+    const accountLogsModel = models.AccountLogsModel;
+    const params = req.params as any;
 
     try {
-        let data = await models.AccontsModel.findAll({
-            where: {
-                id: params.id,
-            },
+        // Fetch accounts and sum income and expenses
+        const data = await models.AccontsModel.findAll({
             include: [
                 {
-                    model: informationsModel,
-                    as: 'student_info',
+                    model: accountLogsModel,
+                    as: 'account_log',
+                    attributes: [
+                        'type',
+                        [fn('SUM', col('amount')), 'totalAmount'],
+                    ],
+                    // group: ['type'], // Grouping by type
                 },
             ],
+            attributes: {
+                include: [
+                    [fn('SUM', col('account_log.amount')), 'totalIncome'],
+                ],
+            },
         });
 
-        if (data) {
-            return response(200, 'data created', data);
-        } else {
-            throw new custom_error('not found', 404, 'data not found');
-        }
+        // Prepare the result with totals for income and expenses
+        const result = {
+            totalIncome: 0,
+            totalExpenses: 0,
+            accounts: data,
+        };
+
+        // Process the account logs to get totals
+        data.forEach((account) => {
+            if (account.account_log) {
+                account.account_log.forEach((log) => {
+                    const totalAmount =
+                        parseFloat(log.getDataValue('totalAmount')) || 0;
+                    if (log.type === 'income') {
+                        result.totalIncome += totalAmount;
+                    } else if (log.type === 'expense') {
+                        result.totalExpenses += totalAmount;
+                    }
+                });
+            }
+        });
+
+        // Return the response with totals
+        return response(200, 'Data retrieved successfully', data);
     } catch (error: any) {
-        let uid = await error_trace(models, error, req.url, req.params);
+        const uid = await error_trace(models, error, req.url, req.params);
         if (error instanceof custom_error) {
             error.uid = uid;
         } else {
