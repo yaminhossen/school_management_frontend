d3abd0be33f8082a4506f48be03af0c0f212f273
Yamin Hossain <streatdot616@gmail.com>
2024-09-23 15:53:29 +0600

    lsfj

---
+++ ledger.js
@@ -0,0 +1,42 @@
+let abcd = {
+    '2024-08-25': [
+        {
+            ac_category_id: 1,
+            title: 'salary',
+            debit: 0,
+            credit: 0,
+        },
+        {
+            ac_category_id: 2,
+            title: 'fees',
+            debit: 0,
+            credit: 0,
+        },
+        {
+            ac_category_id: 3,
+            title: 'transport',
+            debit: 0,
+            credit: 0,
+        },
+    ],
+    '2024-08-26': [
+        {
+            ac_category_id: 1,
+            title: 'salary',
+            debit: 0,
+            credit: 0,
+        },
+        {
+            ac_category_id: 2,
+            title: 'fees',
+            debit: 0,
+            credit: 0,
+        },
+        {
+            ac_category_id: 3,
+            title: 'transport',
+            debit: 0,
+            credit: 0,
+        },
+    ],
+};
