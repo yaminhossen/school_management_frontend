7c485c4059e0cc62a386d9c71bc60af13993ad90
Yamin Hossain <streatdot616@gmail.com>
2024-09-29 18:19:28 +0600

    osdf

--- public/management/parent/views/pages/notices/config/routes.tsx
+++ public/management/parent/views/pages/notices/config/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from './Layout';
 import Index from '../Index';
+import NoticeDetails from '../pages/NoticeDetails';
 
 // export { default as DashboardCounterAll} from "./All.jsx";
 
@@ -12,6 +13,10 @@ export default {
             path: '',
             index: true,
             element: <Index />,
+        },
+        {
+            path: 'details/:id',
+            element: <NoticeDetails />,
         },
     ],
 };
