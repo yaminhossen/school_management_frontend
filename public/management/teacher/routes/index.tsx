91a9dbefad2f51f2f2fc459f3ff45eddd06f7874
Yamin Hossain <streatdot616@gmail.com>
2024-12-02 13:28:51 +0600

    staff profile

--- public/management/teacher/routes/index.tsx
+++ public/management/teacher/routes/index.tsx
@@ -3,23 +3,8 @@ import DashboardLayout from '../views/layouts/DashboardLayout';
 import T1 from '../views/pages/T1';
 import user_branch_staff_routes from '../views/pages/users/config/routes';
 import React from 'react';
-import Class from '../views/menu_pages/Class';
-import ClassRoutine from '../views/menu_pages/ClassRoutine';
-import Profile from '../views/menu_pages/Profile';
-import ExamRoutine from '../views/menu_pages/ExamRoutine';
-import MarkSheet from '../views/menu_pages/MarkSheet';
-import Attendance from '../views/menu_pages/Attendance';
-import Students from '../views/menu_pages/Students';
-import Assignment from '../views/menu_pages/Assignment';
-import CourseMaterial from '../views/menu_pages/CourseMaterial';
-import Assesment from '../views/menu_pages/Assesment';
-import Reports from '../views/menu_pages/reports';
-import Settings from '../views/menu_pages/Settings';
 import Analytics from '../views/menu_pages/Analytics';
-import LeaveApplications from '../views/menu_pages/LeaveApplications';
-import AcademicResources from '../views/menu_pages/AcademicResources';
 import MyActivities from '../views/menu_pages/MyActivities';
-import Notice from '../views/menu_pages/Notices';
 
 import profile_routes from '../views/pages/profile/config/routes';
 import class_routine_routes from '../views/pages/class_routine/config/routes';
@@ -64,30 +49,6 @@ const router: RouteTypes[] = [
                 path: '',
                 element: <T1 />,
             },
-            // {
-            //     path: 'classes',
-            //     element: <Class />,
-            // },
-            // {
-            //     path: 'assignments',
-            //     element: <Assignment />,
-            // },
-            // {
-            //     path: 'course-materials',
-            //     element: <CourseMaterial />,
-            // },
-            // {
-            //     path: 'assesments',
-            //     element: <Assesment />,
-            // },
-            // {
-            //     path: 'reports',
-            //     element: <Reports />,
-            // },
-            // {
-            //     path: 'settings',
-            //     element: <Settings />,
-            // },
             {
                 path: 'analytics',
                 element: <Analytics />,
