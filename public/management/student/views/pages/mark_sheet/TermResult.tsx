ff77ee2c36b9aba56fab792a4c0bcd2aa94c9691
Yamin Hossain <streatdot616@gmail.com>
2025-01-27 17:58:22 +0600

    student result print page design

--- public/management/student/views/pages/mark_sheet/TermResult.tsx
+++ public/management/student/views/pages/mark_sheet/TermResult.tsx
@@ -22,10 +22,8 @@ const TermResult: React.FC<Props> = (props: Props) => {
                 `/api/v1/exam-student-marks/exam-wise/${termid}/${classid}`,
             );
             let newdata = Object.values(response.data.data)[0];
-            // console.log(newdata);
             setData(newdata);
             setTitle(Object.keys(response.data.data)[0]);
-            // setData(response.data);
         } catch (error) {
             setError(error);
         }
@@ -34,7 +32,6 @@ const TermResult: React.FC<Props> = (props: Props) => {
     useEffect(() => {
         fetchData();
     }, []);
-    // console.log(data);
 
     function getGrade(score) {
         if (score >= 30 && score <= 39) {
@@ -58,20 +55,189 @@ const TermResult: React.FC<Props> = (props: Props) => {
     function convertUnderscoreToSpace(str: string): string {
         return str.replace(/_/g, ' ');
     }
+    // Handle the print functionality
+
     return (
         <div className="admin_dashboard">
-            <div>{/* <h3>Current CGPA : 4.50</h3> */}</div>
             <h3 className="table_heading">{convertUnderscoreToSpace(title)}</h3>
             {/* <h3 className="table_heading student_semister">Result History</h3> */}
+
             <div className="content_body ">
                 <div className="data_list mb-4">
                     <div className="table_responsive custom_scroll">
+                        <table className="outer-table">
+                            <tr>
+                                <td className="top-cell">
+                                    <h1>NURUL HIQMAH MODEL MADRASA</h1>
+                                </td>
+                            </tr>
+                            <tr className="middle_table">
+                                <td style={{ padding: 0 }}>
+                                    <table className="inner-table">
+                                        <tr>
+                                            <td className="side-cell">
+                                                <div className="student_image">
+                                                    <img
+                                                        src="/assets/dashboard_uni/1.jpg"
+                                                        alt=""
+                                                    />
+                                                </div>
+                                            </td>
+                                            <td className="middle-cell">
+                                                <div>
+                                                    <p>Sayadpur, Rangpur</p>
+                                                    <div className="logo_image">
+                                                        <img
+                                                            src="/assets/dashboard_uni/logo1.png"
+                                                            alt=""
+                                                        />
+                                                    </div>
+                                                    <h3>
+                                                        {/* <u> */}
+                                                        ACADEMIC TRANSCRIPT
+                                                        {/* </u> */}
+                                                    </h3>
+                                                </div>
+                                            </td>
+                                            <td className="side-cell">
+                                                <table className="grade_sheet">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Range</th>
+                                                            <th>Grade</th>
+                                                            <th>GPA</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>80-100</td>
+                                                            <td>A+</td>
+                                                            <td>5.0</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>70-79</td>
+                                                            <td>A</td>
+                                                            <td>4.0</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>60-69</td>
+                                                            <td>A-</td>
+                                                            <td>3.5</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>50-59</td>
+                                                            <td>B</td>
+                                                            <td>3.0</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>40-49</td>
+                                                            <td>C</td>
+                                                            <td>2.0</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>33-39</td>
+                                                            <td>D</td>
+                                                            <td>1.0</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>00-32</td>
+                                                            <td>F</td>
+                                                            <td>0.0</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style={{ padding: 0 }}>
+                                    <table className="inner-table">
+                                        <tr>
+                                            <td className="bottom-left">
+                                                <table className="result_table">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Branch</td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                Uttora
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                Admission no
+                                                            </td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                13lsjdf
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Roll no</td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                sdf2323234
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Date</td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                {moment(
+                                                                    data
+                                                                        ?.student_info
+                                                                        ?.admission_date,
+                                                                ).format(
+                                                                    'YYYY-MM-DD',
+                                                                )}
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                            <td className="bottom-right">
+                                                <table className="result_table2">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Branch</td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                Uttora
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                Admission no
+                                                            </td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                13lsjdf
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>
+                                                                Admission no
+                                                            </td>
+                                                            <td>:</td>
+                                                            <td className="font-medium text-dark-medium">
+                                                                13lsjdf
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
                         <table>
                             <thead>
                                 <tr>
                                     <th></th>
-                                    <th>id</th>
-                                    <th>Exam</th>
+                                    {/* <th>id</th> */}
                                     <th>Subject</th>
                                     <th>Marks</th>
                                     <th>Grade</th>
@@ -83,18 +249,9 @@ const TermResult: React.FC<Props> = (props: Props) => {
                                         return (
                                             <tr>
                                                 <td></td>
-                                                <td>{index + 1}</td>
-                                                <td>{i.exams?.title}</td>
+                                                {/* <td>{index + 1}</td> */}
                                                 <td>{i?.subject?.name}</td>
                                                 <td>{i?.obtained_mark}</td>
-                                                {/* <td>
-                                                    <Link
-                                                        className="btn btn-sm btn-outline-info"
-                                                        to={`/mark-sheet/details/${i.id}`}
-                                                    >
-                                                        details
-                                                    </Link>
-                                                </td> */}
                                                 <td>
                                                     {getGrade(i.obtained_mark)}
                                                 </td>
@@ -107,37 +264,6 @@ const TermResult: React.FC<Props> = (props: Props) => {
                     </div>
                 </div>
             </div>
-            {/* <h3 className="table_heading student_semister">Second Semester</h3>
-            <div className="content_body ">
-                <div className="data_list">
-                    <div className="table_responsive custom_scroll">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>id</th>
-                                    <th>Subject</th>
-                                    <th>Marks</th>
-                                    <th>Grade</th>
-                                </tr>
-                            </thead>
-                            <tbody id="all_list">
-                                {datas?.map((i: { [key: string]: any }) => {
-                                    return (
-                                        <tr>
-                                            <td></td>
-                                            <td>{i.id}</td>
-                                            <td>{i.subject}</td>
-                                            <td>{i.marks}</td>
-                                            <td>{i.grade}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 };
