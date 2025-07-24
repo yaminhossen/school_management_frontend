b43c31fae6e57880eb776a0b01301a78744bf212
Yamin Hossain <streatdot616@gmail.com>
2025-07-24 17:05:24 +0600

    lsdfj

--- public/management/student/views/pages/profile/pages/Parent.tsx
+++ public/management/student/views/pages/profile/pages/Parent.tsx
@@ -56,82 +56,83 @@ const Details: React.FC<Props> = (props: Props) => {
                     <div className="single_student_details">
                         <div>
                             <h4>Guardians</h4>
-                                            <div className="basic_info mb-4 ">
-                                                <table className="table text-nowrap student_table">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>Name</td>
-                                                            <td>:</td>
-                                                            <td className="font-medium text-dark-medium">
-                                                                {
-                                                                    data.parents?.parent_details
-                                                                        ?.name
-                                                                }
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Email</td>
-                                                            <td>:</td>
-                                                            <td className="font-medium text-dark-medium">
-                                                                {
-                                                                    data.parents?.parent_details
-                                                                        ?.email
-                                                                }
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Relation</td>
-                                                            <td>:</td>
-                                                            <td className="font-medium text-dark-medium">
-                                                                {data.parents?.parent_details
-                                                                        ?.relation}
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                                <table className="table text-nowrap student_table ml-2">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>
-                                                                Phone Number
-                                                            </td>
-                                                            <td>:</td>
-                                                            <td className="font-medium text-dark-medium">
-                                                                {
-                                                                    data.parents?.parent_details
-                                                                        ?.phone_number
-                                                                }
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Status</td>
-                                                            <td>:</td>
-                                                            <td className="font-medium text-dark-medium">
-                                                                {
-                                                                    data.parents?.parent_details
-                                                                        ?.status
-                                                                }
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Picture</td>
-                                                            <td>:</td>
-                                                            <td className="font-medium text-dark-medium">
-                                                                <a
-                                                                    href={
-                                                                        data.parents?.parent_details
-                                                                            ?.image || undefined
-                                                                    }
-                                                                    target="_blank"
-                                                                    rel="noopener noreferrer"
-                                                                >
-                                                                    Show Image
-                                                                </a>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
+                            <div className="basic_info mb-4 ">
+                                <table className="table text-nowrap student_table">
+                                    <tbody>
+                                        <tr>
+                                            <td>Name</td>
+                                            <td>:</td>
+                                            <td className="font-medium text-dark-medium">
+                                                {
+                                                    data.parents?.parent_details
+                                                        ?.name
+                                                }
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Email</td>
+                                            <td>:</td>
+                                            <td className="font-medium text-dark-medium">
+                                                {
+                                                    data.parents?.parent_details
+                                                        ?.email
+                                                }
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Relation</td>
+                                            <td>:</td>
+                                            <td className="font-medium text-dark-medium">
+                                                {
+                                                    data.parents?.parent_details
+                                                        ?.relation
+                                                }
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table className="table text-nowrap student_table ml-2">
+                                    <tbody>
+                                        <tr>
+                                            <td>Phone Number</td>
+                                            <td>:</td>
+                                            <td className="font-medium text-dark-medium">
+                                                {
+                                                    data.parents?.parent_details
+                                                        ?.phone_number
+                                                }
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Status</td>
+                                            <td>:</td>
+                                            <td className="font-medium text-dark-medium">
+                                                {
+                                                    data.parents?.parent_details
+                                                        ?.status
+                                                }
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Picture</td>
+                                            <td>:</td>
+                                            <td className="font-medium text-dark-medium">
+                                                <a
+                                                    href={
+                                                        data.parents
+                                                            ?.parent_details
+                                                            ?.image || undefined
+                                                    }
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    Show Image
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 )}
